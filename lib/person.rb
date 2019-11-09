5d42f838d4ef4ed011f77464408aa0d342bd50d7
Kevin Downs <kevin.downs04@gmail.com>
2019-11-09 18:40:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,20 @@
 
 class Person 
 
-def name=()
+def name=(person_name)
+  @name = person_name
 end
+
+def name 
+  @name
+end
+
+def job=(person_job)
+  @job = person_job
+end
+
+def job 
+  @job
+end
+
 end
